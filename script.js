af4a3a2353dada73215098979854798504aca29e
Ishu Baghel <baghelishu09@gmail.com>
2024-11-08 09:26:47 +0530

    modified

---
+++ script.js
@@ -0,0 +1,9 @@
+let x=document.getElementById('loginbtn');
+x.addEventListener('click',function(){
+    let name=document.getElementById('name');
+    let email=document.getElementById('email');
+    let mob=document.getElementById('mobile no');
+    name.style.display='none';
+    email.style.display='none';
+    mob.style.display='none';
+});
