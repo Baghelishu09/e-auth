a364642d8f121381df0125e94c2da0ceec68d4df
Ishu Baghel <baghelishu09@gmail.com>
2024-11-23 00:57:25 +0530

    mongoose connect

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const userModel = require("./users");
+//const bcrypt = require("bcrypt");
 require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/submit", async function (req, res) {
   const { name, uname, email, mobileNo, password } = req.body;
+//  const hashedPassword = await bcrypt.hash(password, 10);
   const user = new userModel({
     name,
     uname,
     email,
     mobileNo,
-    password,
+    password//: hashedPassword
   });
-
   try {
+    if (await userModel.findOne({ email: email })) {
+      console.log("Email already exists!");
+      res.json({ status: "failed", message: "Email already exists!" });
+      return;
+    }
     await user.save();
-    console.log(`${name} registered successfully!`)
-    res.send("User registered successfully");
+    res.json({ status: "success", message: `${name} registered successfully` });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ status: "failed" });
   }
 });
 
--- public/script.js
+++ public/script.js
@@ -1,77 +1,60 @@
-// Define global variables for form fields in the signup form
-let fullname = document.getElementById("name");
-let email = document.getElementById("email");
-let mob = document.getElementById("mobileNo");
-let pswd = document.getElementById("password");
-let uname = document.getElementById("uname");
+function signup(event){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("mobileNo").value;
+    var uname = document.getElementById("uname").value;
+    const regex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
-// Validation function for signup form
-function validate(event) {
-  // Prevent the form from submitting normally to handle custom validation
-  event.preventDefault();
-  const nameval = document.getElementById("name").value;
-  const emailval = document.getElementById("email").value;
-  const mobval = document.getElementById("mobileNo").value;
-  const pswdval = document.getElementById("password").value;
-  const unameval = document.getElementById("uname").value;
+    event.preventDefault();
 
-  // form validation
-
-  let errors = [];
-
-  if (
-    nameval == "" ||
-    emailval == "" ||
-    mobval == "" ||
-    pswdval == "" ||
-    unameval == ""
-  ) {
-    errors.push(`All fields are mandatory`);
-  } else if (pswdval.length < 6) {
-    errors.push("Password must be atleast 6 characters long");
-  } else if (mobval.length != 10) {
-    errors.push("Mobile number must be 10 digits long");
-  } else if (emailval.indexOf("@") <= 0) {
-    errors.push("Invalid email");
-  }
-  if (errors.length > 0) {
-    alert(errors.join("\n"));
-  } else {
-    const formData = {
-      name: nameval,
-      uname: unameval,
-      email: emailval,
-      mobileNo: mobval,
-      password: pswdval,
-    };
-    fetch("/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams(formData),
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        alert(data);
-        fullname.value = "";
-        email.value = "";
-        mob.value = "";
-        pswd.value = "";
-        uname.value = "";
-        document.getElementById("signup-container").style.display = "none";
-        document.getElementById("login_form").style.display = "block";
-      });
-  }
+    if(email == "" || password == "" || name == "" || phone == "" || uname == ""){
+        alert("Please fill all the fields");
+    }
+    else if(password.length < 6 && password.length > 20){
+        alert("Password must be atleast 6 characters long");
+    }
+    else if(phone.length != 10){
+        alert("Enter a valid phone number");
+    }
+    else if(!email.match(regex)){
+        alert("Enter a valid email");
+    }
+    else{
+       var data ={
+            email: email,
+            password: password,
+            name: name,
+            phone: phone,
+            uname: uname
+       }
+       fetch('/submit',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            ,body: JSON.stringify(data),
+       })
+       .then(response => response.json())
+       .then(data => {
+         if(data.status == "success"){
+            email = "";
+            uname = "";
+            password = "";
+            name = "";
+            phone = "";
+            document.getElementById("signup-container").style.display = "none";
+            document.getElementById("login_form").style.display = "block";
+         }
+         else{
+            alert(data.message)
+         }
+       })
+    }
+    
 }
-function loginbtn(event) {
-  document.getElementById("signup-container").style.display = "none";
-  document.getElementById("login_form").style.display = "block";
-}
-function registertosignup() {
-  document.getElementById("signupbtn").addEventListener("click", function () {
-    document.getElementById("login_form").style.display = "none";
-    document.getElementById("signup-container").style.display = "block";
-  });
-}
-registertosignup();
+document.getElementById("register").addEventListener("click", signup);
+document.getElementById("loginbtn2").addEventListener("click", function(){
+    document.getElementById("signup-container").style.display = "none";
+    document.getElementById("login_form").style.display = "block";
+});
