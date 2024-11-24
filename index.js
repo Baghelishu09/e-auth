b74cbf55d3a868fce28c91f33c240d8dbba80be8
Ishu Baghel <baghelishu09@gmail.com>
2024-11-24 19:43:51 +0530

    modified

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const userModel = require("./users");
-//const bcrypt = require("bcrypt");
-require('dotenv').config();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Registration
+
 app.post("/submit", async function (req, res) {
   const { name, uname, email, mobileNo, password } = req.body;
-//  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
   const user = new userModel({
     name,
     uname,
     email,
     mobileNo,
-    password//: hashedPassword
+    password: hashedPassword,
   });
   try {
     if (await userModel.findOne({ email: email })) {
@@ -30,6 +32,26 @@ app.post("/submit", async function (req, res) {
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: "failed" });
+  }
+});
+
+//Login
+
+app.post("/login", async function (req, res) {
+  const { uname_login, password_login } = req.body;
+  const user = await userModel.findOne({ uname: uname_login });
+  if (user) {
+    const isMatch = await bcrypt.compare(password_login, user.password);
+    if (!isMatch) {
+      res.json({ status: "failed", message: "Something went wrong" });
+    } else {
+      res.json({
+        status: "success",
+        message: `${user.name} logged in successfully`,
+      });
+    }
+  } else {
+    res.json({ status: "failed", message: "User not found! Register" });
   }
 });
 
--- public/script.js
+++ public/script.js
@@ -1,61 +1,98 @@
-function signup(event){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var name = document.getElementById("name").value;
-    var phone = document.getElementById("mobileNo").value;
-    var uname = document.getElementById("uname").value;
-    const regex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+function signup(event) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("mobileNo").value;
+  var uname = document.getElementById("uname").value;
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    event.preventDefault();
+  event.preventDefault();
 
-    if(email == "" || password == "" || name == "" || phone == "" || uname == ""){
-        alert("Please fill all the fields");
-    }
-    else if(password.length < 6 && password.length > 20){
-        alert("Password must be atleast 6 characters long");
-    }
-    else if(phone.length != 10){
-        alert("Enter a valid phone number");
-    }
-    else if(!email.match(regex)){
-        alert("Enter a valid email");
-    }
-    else{
-       var data ={
-            email: email,
-            password: password,
-            name: name,
-            phone: phone,
-            uname: uname
-       }
-       fetch('/submit',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-            ,body: JSON.stringify(data),
-       })
-       .then(response => response.json())
-       .then(data => {
-         if(data.status == "success"){
-            alert(data.message);
-            email = "";
-            uname = "";
-            password = "";
-            name = "";
-            phone = "";
-            document.getElementById("signup-container").style.display = "none";
-            document.getElementById("login_form").style.display = "block";
-         }
-         else{
-            alert(data.message)
-         }
-       })
-    }
-    
+  if (
+    email == "" ||
+    password == "" ||
+    name == "" ||
+    phone == "" ||
+    uname == ""
+  ) {
+    alert("Please fill all the fields");
+  } else if (password.length < 6 && password.length > 20) {
+    alert("Password must be atleast 6 characters long");
+  } else if (phone.length != 10) {
+    alert("Enter a valid phone number");
+  } else if (!email.match(regex)) {
+    alert("Enter a valid email");
+  } else {
+    var data = {
+      email: email,
+      password: password,
+      name: name,
+      phone: phone,
+      uname: uname,
+    };
+    fetch("/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status == "success") {
+          alert(data.message);
+          email = "";
+          uname = "";
+          password = "";
+          name = "";
+          phone = "";
+          document.getElementById("signup-container").style.display = "none";
+          document.getElementById("login_form").style.display = "block";
+        } else {
+          alert(data.message);
+        }
+      });
+  }
 }
 document.getElementById("register").addEventListener("click", signup);
-document.getElementById("loginbtn2").addEventListener("click", function(){
-    document.getElementById("signup-container").style.display = "none";
-    document.getElementById("login_form").style.display = "block";
+document.getElementById("loginbtn2").addEventListener("click", function () {
+  document.getElementById("signup-container").style.display = "none";
+  document.getElementById("login_form").style.display = "block";
+});
+
+function login(event) {
+  console.log("login");
+  var uname_login = document.getElementById("uname_login").value;
+  var password_login = document.getElementById("password_login").value;
+  if (uname_login == "" || password_login == "") {
+    alert("Please fill all the fields");
+    return;
+  } else {
+    var data = {
+      uname_login: uname_login,
+      password_login: password_login,
+    };
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status == "success") {
+          alert(data.message);
+        } else {
+          alert(data.message);
+        }
+      });
+    uname_login == "";
+    password_login == "";
+  }
+}
+document.getElementById("loginbtn").addEventListener("click", login);
+document.getElementById("signupbtn").addEventListener("click", function () {
+  document.getElementById("signup-container").style.display = "block";
+  document.getElementById("login_form").style.display = "none";
 });
